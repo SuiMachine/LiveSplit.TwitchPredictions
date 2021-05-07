613138f71b28d2921c82c50b269b180b92f1f1c0
Sui / SuicideMachine <SuiMachine@users.noreply.github.com>
2021-05-08 01:30:25 +0200

    Yeah, I don't think we'll be doing it (cause it requires secret and we can't have secret available publicaly :(

--- HTTPServer.cs
+++ HTTPServer.cs
@@ -11,17 +11,19 @@ namespace LiveSplit.TwitchPredictions
 {
 	public class HTTPServer
 	{
-		string REDIRECT_URI = "http://localhost:43628/auth/twitch/callback";
+
+		const string REDIRECT_URI = "http://localhost:43628/auth/twitch/callback/";
 		HttpListener listener;
 		Thread t;
 		public bool Terminate;
+		System.Timers.Timer scheduledTermination;
 
 		public static bool IsSupported() => HttpListener.IsSupported;
 
 		public HTTPServer()
 		{
 			System.Diagnostics.Process.Start("https://id.twitch.tv/oauth2/authorize" +
-				"?response_type=token" +
+				"?response_type=code" +
 				"&client_id=" + TwitchConnection.ClientID +
 				"&redirect_uri=" + REDIRECT_URI +
 				"&force_verify=true" +
@@ -36,19 +38,14 @@ namespace LiveSplit.TwitchPredictions
 			listener.Prefixes.Add("http://localhost:43628/");
 			listener.Start();
 			IAsyncResult result = listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
-			System.Timers.Timer scheduledTermination = new System.Timers.Timer(1000 * 60 * 2);
+			scheduledTermination = new System.Timers.Timer(1000 * 60 * 2);
 			scheduledTermination.Elapsed += (o, e) => { CloseHttpListener(); };
 			scheduledTermination.Start();
 		}
 
-		private void ScheduledTermination_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+		public void ListenerCallback(IAsyncResult result)
 		{
-			throw new NotImplementedException();
-		}
-
-		public static void ListenerCallback(IAsyncResult result)
-		{
-			HttpListener listener = (HttpListener)result.AsyncState;
+			listener = (HttpListener)result.AsyncState;
 			HttpListenerContext context = listener.EndGetContext(result);
 			HttpListenerRequest request = context.Request;
 			HttpListenerResponse response = context.Response;
@@ -57,14 +54,47 @@ namespace LiveSplit.TwitchPredictions
 			response.ContentLength64 = buffer.Length;
 			System.IO.Stream output = response.OutputStream;
 			output.Write(buffer, 0, buffer.Length);
-			output.Close();
+
+			var requestUrl = request.Url.ToString();
+			CloseHttpListener();
+			if (!requestUrl.StartsWith(REDIRECT_URI, StringComparison.InvariantCultureIgnoreCase))
+				throw new FailedToGetProperResponseException("Response URL was: " + requestUrl);
+
+			var anchor = requestUrl.Remove(0, REDIRECT_URI.Length);
+			var elements = anchor.Split(new char[] { '?', '&' }, StringSplitOptions.RemoveEmptyEntries);
+			var keyvaluePair = elements.Select(x => x.Split(new char[] { '=' }, 2)).ToDictionary(split => split[0], split => split[1]);
+			if (keyvaluePair["code"] == null)
+				throw new AnchorDoesntContainCodeException();
+
+
 		}
 
 		private void CloseHttpListener()
 		{
 			if (listener != null && listener.IsListening)
 				listener.Close();
-		}
 
+			if (scheduledTermination != null)
+			{
+				scheduledTermination.Stop();
+				scheduledTermination.Dispose();
+			}
+		}
+	}
+
+	public class FailedToGetProperResponseException : Exception
+	{
+		public FailedToGetProperResponseException() : base()	{}
+
+		public FailedToGetProperResponseException(string message) : base(message) { }
+		public FailedToGetProperResponseException(string message, Exception innerException) : base(message, innerException) { }
+	}
+
+	public class AnchorDoesntContainCodeException : Exception
+	{
+		public AnchorDoesntContainCodeException() : base() { }
+
+		public AnchorDoesntContainCodeException(string message) : base(message) { }
+		public AnchorDoesntContainCodeException(string message, Exception innerException) : base(message, innerException) { }
 	}
 }
